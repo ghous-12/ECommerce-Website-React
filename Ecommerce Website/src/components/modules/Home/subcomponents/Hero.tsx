96945556a86b3cc56644a617c02824bd21142fc8
Ghous <sghous081@gmail.com>
2024-11-15 00:08:57 +0500

    [Feature]: Hero section - designed the basic of hero section

--- Ecommerce Website/src/components/modules/Home/subcomponents/Hero.tsx
+++ Ecommerce Website/src/components/modules/Home/subcomponents/Hero.tsx
@@ -4,7 +4,16 @@ import { images } from "../../../../utils/constants";
 const Hero = () => {
   return (
     <>
-      <img src={images.Hero} alt="" className="" />=
+      <div>
+        <img src={images.Hero} alt="" className="" />
+        {/* heading */}
+        <h1 className="text-5xl font-bold absolute top-60 left-40 text-white">
+          Genuine
+        </h1>
+        <h1 className="text-5xl font-bold absolute top-80 left-40 text-white">
+          Shoes Online Shopping
+        </h1>
+      </div>
     </>
   );
 };
